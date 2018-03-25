2b89eeb991e49551f956964fb63cabd3220b7a1c
jijiefengmyy <yuanyuanma@163.com>
2018-03-25 22:36:47 +0800

    redis

---
+++ src/test/java/com/myy/intellij_gradle_spboot_redis/IntellijGradleSpbootRedisApplicationTests.java
@@ -0,0 +1,16 @@
+package com.myy.intellij_gradle_spboot_redis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IntellijGradleSpbootRedisApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
